2645f491b53c115fa94f072cc23f3ac1223857a8
bloisematias-netizen <bloisematias@gmail.com>
2026-01-02 15:42:05 -0300

    fix icons

---
+++ app/src/main/java/io/github/bloisematias_netizen/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.bloisematias_netizen.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
